ce2d0440a75e1abd2e72265614eff3cbd4555dc2
rodrigobuono63 <160273522+rodrigobuono63@users.noreply.github.com>
2024-06-10 09:45:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,29 +6,58 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado:"Como podemos reduzir a poluição do ar nas grandes cidades?",
+        enunciado: "Como podemos reduzir a poluição do ar nas grandes cidades?",
         alternativas: [
-            " Promover o uso de transportes públicos e alternativos: Incentivar a utilização de ônibus, metrôs, bicicletas e caminhadas através de melhorias na infraestrutura e criação de campanhas de conscientização ",
-            " Implementar zonas de baixa emissão: Criar áreas onde o tráfego de veículos altamente poluentes seja restrito ou proibido, incentivando o uso de veículos elétricos e híbridos "
+            {
+                texto: "a)  Promover o uso de transportes públicos e alternativos: Incentivar a utilização de ônibus, metrôs, bicicletas e caminhadas através de melhorias na infraestrutura e criação de campanhas de conscientização ",
+                afirmacao: "Errado!"
+            },
+            {
+                texto: "b)  Promover o uso de transportes públicos e alternativos: Incentivar a utilização de ônibus, metrôs, bicicletas e caminhadas através de melhorias na infraestrutura e criação de campanhas de conscientização ",
+                afirmacao: "Correto!"
+            }
         ]
     },
 ];
 
+
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
-function mostraAlternativas() {
+
+function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativa.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + "";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
 mostraPergunta();
+
 
