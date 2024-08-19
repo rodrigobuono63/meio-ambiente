bf20c33cbe27079000a92eb5666007865567f740
rodrigobuono63 <160273522+rodrigobuono63@users.noreply.github.com>
2024-08-19 09:56:01 -0300

    perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -3,14 +3,14 @@ export const perguntas = [
         enunciado: "Como podemos reduzir a poluição do ar nas grandes cidades?",
         alternativas: [
             {
-                texto: "a)Promover o uso de transportes públicos e alternativos: Incentivar a utilização de ónibus, metros, bicicletas e caminhadas através de melhorias na infraestrutura e criação de campanhas de conscientização. ",
+                texto: "Promover o uso de transportes públicos e alternativos: Incentivar a utilização de ónibus, metros, bicicletas e caminhadas através de melhorias na infraestrutura e criação de campanhas de conscientização. ",
                 afirmacao:[
                  "Errado!",
                 ""],
                 proxima: 1,
             },
             {
-                texto: "b)Implementar zonas de baixa emissão: Criar áreas onde o tráfego de veiculos altamente poluentes seja restrito ou proibido, incentivando o uso de veículos elétricos e hibridos. ",
+                texto: "Implementar zonas de baixa emissão: Criar áreas onde o tráfego de veiculos altamente poluentes seja restrito ou proibido, incentivando o uso de veículos elétricos e hibridos. ",
                 afirmacao:[
                  "Correto!",
                 ""]
